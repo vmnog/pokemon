a71397d80dcaf0f6badfe0db6dc4671b3719e641
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-07-05 21:02:41 -0300

    config: configuring roboto font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
+
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | pokemon</title>
+      </Head>
+      <h1 className={styles.title}>Hello</h1>
+    </>
+  );
 }
